e086175bf4ffab914a32701aa4190f158df0ca84
Thalita <dias.t@husky.neu.edu>
2017-09-26 11:37:42 -0400

    Delete pkill.sh

--- pkill.sh
+++
@@ -1,6 +0,0 @@
-
-ps -ef | grep -i chromedriver | grep tcoleman | awk '{print $2}' | xargs kill
-ps -ef | grep -i chromium-browser | grep tcoleman | awk '{print $2}' | xargs kill
-ps -ef | grep -i webdriver | grep tcoleman | awk '{print $2}' | xargs kill
-ps -ef | grep -i xvfb | grep tcoleman | awk '{print $2}' | xargs kill
-
